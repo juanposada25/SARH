6c95e44cbdf15be01263e1b639b671f2ba3dfaa9
juanposada25 <juandiegoposada245@gmail.com>
2025-04-11 17:24:12 -0500

    se añadio el inicio de sesion a todas las funciones de la pagina

--- resources/views/empleado/index.blade.php
+++ resources/views/empleado/index.blade.php
@@ -9,6 +9,47 @@
   </head>
   <body>
 
+     <!-- Navbar -->
+     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <ul class="navbar-nav ms-auto">
+                @auth
+                    <li class="nav-item">
+                        <a class="nav-link" href="/">Inicio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/empleados">Empleado</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/departamentos">Departamento</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/asistencias">Asistencia</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">
+                            {{ Auth::user()->name }}
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-end">
+                            <li><a class="dropdown-item" href="{{ route('profile.edit') }}">Perfil</a></li>
+                            <li>
+                                <form method="POST" action="{{ route('logout') }}">
+                                    @csrf
+                                    <button type="submit" class="dropdown-item">Cerrar sesión</button>
+                                </form>
+                            </li>
+                        </ul>
+                    </li>
+                @else
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">Iniciar sesión</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('register') }}">Registrarse</a>
+                    </li>
+                @endauth
+            </ul>
+    </nav>
+
     <div class="container">
         <h1 class="mt-4">Listado de Empleados</h1>
 
